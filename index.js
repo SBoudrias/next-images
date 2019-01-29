dd9c4e7f57ea92de7e333a4663ebea898d4ebe22
Simon Boudrias <admin@simonboudrias.com>
2019-01-29 10:37:19 +0800

    Allow public-path to be dynamic Next.js assetPrefix

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ module.exports = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
       const { isServer } = options;
-      nextConfig = Object.assign({ inlineImageLimit: 8192, assetPrefix: "" }, nextConfig);
+      nextConfig = Object.assign({ inlineImageLimit: 8192 }, nextConfig);
 
       if (!options.defaultLoaders) {
         throw new Error(
-          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
-        )
+          "This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
+        );
       }
 
       config.module.rules.push({
@@ -18,19 +18,18 @@ module.exports = (nextConfig = {}) => {
             options: {
               limit: nextConfig.inlineImageLimit,
               fallback: "file-loader",
-              publicPath: `${nextConfig.assetPrefix}/_next/static/images/`,
-              outputPath: `${isServer ? "../" : ""}static/images/`,
+              outputPath: "static/images/",
               name: "[name]-[hash].[ext]"
             }
           }
         ]
       });
 
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
       }
 
-      return config
+      return config;
     }
-  })
-}
+  });
+};
